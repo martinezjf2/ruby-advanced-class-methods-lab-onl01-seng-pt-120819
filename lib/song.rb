8629c893941c1a8d7f6e53e23d46ef1193cda0cc
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-18 16:55:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,16 @@ class Song
 
   def self.find_by_name(name)
     @@all.detect{ |song| song.name == name }
-
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #    self.find_by_name(name)
+    # else
+    #   self.create_by_name(name)
+    # end
+    #
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
