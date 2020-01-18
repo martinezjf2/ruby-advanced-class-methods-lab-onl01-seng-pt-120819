984650298ccbf0e2d888ea6cda5c314363738dea
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-18 17:05:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,8 @@ class Song
 
   def self.alphabetical
     # @@all.each.name.sort{ |a,b| a.name <=> b.name }
-    @@all.sort{ |a,b| a.name <=> b.name }
-
+    # @@all.sort{ |a,b| a.name <=> b.name }
+    @@all.sort_by{ |song| song.name }
   end
 
   def self.new_from_filename
